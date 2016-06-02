0de428ed5b2d3221596e590e5dda95e77b08d7e9
Deniz Mekik <denizmekik@gmail.com>
2016-06-01 17:08:31 -0700

    add comment

--- helloWorld.go
+++ helloWorld.go
@@ -1,44 +1,47 @@
+// Golang Rest API server. It runs on localhost at port 8080
+// Simply servers post requests are sent with json data at '/hi' endpoint
+
 package main
 
 import (
-  "encoding/json"
-  "net/http"
-  "log"
+	"encoding/json"
+	"log"
+	"net/http"
 )
 
-type User struct{
-  FirstName string
-  LastName string
+type User struct {
+	FirstName string
+	LastName  string
 }
 
-type GreetUser struct{
-  Body string
+type GreetUser struct {
+	Body string
 }
 
-func HiHandler( w http.ResponseWriter, r *http.Request) {
-  var u User
-  // checks the request body
-  if r.Body == nil {
-      http.Error(w, "Please send a request body", 400)
-      return
-  }
-  // checks json
-  err := json.NewDecoder(r.Body).Decode(&u)
-  if err != nil {
-      http.Error(w, err.Error(), 400)
-      return
-  }
-  // checks fields in json 
-  if u.FirstName == "" || u.LastName == "" {
-    http.Error(w, "Please fill both fields", 400)
-    return
-  }
-  // sends back json
-  json.NewEncoder(w).Encode(GreetUser{"Hi " + u.FirstName + " " + u.LastName})
+func HiHandler(w http.ResponseWriter, r *http.Request) {
+	var u User
+	// checks the request body
+	if r.Body == nil {
+		http.Error(w, "Please send a request body", 400)
+		return
+	}
+	// checks json
+	err := json.NewDecoder(r.Body).Decode(&u)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+	// checks fields in json
+	if u.FirstName == "" || u.LastName == "" {
+		http.Error(w, "Please fill both fields", 400)
+		return
+	}
+	// sends back json
+	json.NewEncoder(w).Encode(GreetUser{"Hi " + u.FirstName + " " + u.LastName})
 
 }
 
 func main() {
-  http.HandleFunc( "/hi", HiHandler )
-  log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/hi", HiHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
